ef31fae57c299f4e98cec07b30126dd566173621
nagysamuel <nagy.leved.samuel@diak.szbi-pg.hu>
2024-10-09 09:48:19 +0200

    add test_calculator.py with subract, multiply, divide function

--- test_calculator.py
+++ test_calculator.py
@@ -13,3 +13,16 @@ def test_subtract():
     assert subtract(-1, -1) == 0
     assert subtract(-1, 1) == -2
     assert subtract(0, 5) == -5
+
+def test_multiply():
+    assert multiply(3, 5) == 15
+    assert multiply(3, -5) == -15 
+    assert multiply(6, 2) == 12
+    assert multiply(10, -10) == -100
+    assert multiply(0, 0) == 0
+
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(10, 5) == 2
+    assert divide(-4, -4) == 1
+    assert divide(20, 5) == 4
